cf4f9cdc5e8974cb3ca60bac48e2df3da949f4de
Oliver Newman <orn688@gmail.com>
2020-12-03 22:26:29 -0500

    Finish day 2, part 2

--- src/aoc_2020/day02.clj
+++ src/aoc_2020/day02.clj
@@ -1,19 +1,19 @@
 (ns aoc-2020.day02
   (:require [clojure.string :as str]))
 
-(deftype PasswordInfo [min-count max-count letter password])
+(deftype PasswordInfo [x y letter password])
 
 (defn create-password-info
   "`1-3 a: abcde` ->
-   password-info{min-count: 1, max-count: 3, letter: `a`, password: `abcde`}"
+   password-info{x: 1, y: 3, letter: `a`, password: `abcde`}"
   [raw-info]
   (let [parts (str/split raw-info #" ")
-        min-max (map #(Integer/parseInt %) (str/split (first parts) #"-"))
-        min-count (first min-max)
-        max-count (second min-max)
+        nums (map #(Integer/parseInt %) (str/split (first parts) #"-"))
+        x (first nums)
+        y (second nums)
         letter (first (second parts))
         password (peek parts)]
-    (PasswordInfo. min-count max-count letter password)))
+    (PasswordInfo. x y letter password)))
 
 (defn parse-input
   [orig-input]
@@ -23,23 +23,42 @@
   [password letter]
   (get (frequencies password) letter 0))
 
-(defn is-valid
-  [password-info]
-  (let [actual-count (count-occurrences (.password password-info) (.letter password-info))]
+(defn is-valid-part1
+  [pwi]
+  (let [actual-count (count-occurrences (.password pwi) (.letter pwi))]
     (and
-     (>= actual-count (.min-count password-info))
-     (<= actual-count (.max-count password-info)))))
+     (>= actual-count (.x pwi))
+     (<= actual-count (.y pwi)))))
 
 (defn part1-impl
-  [password-infos]
-  (count (filter is-valid password-infos)))
+  [pwis]
+  (count (filter is-valid-part1 pwis)))
 
 (defn part1
-  "Given a list of passwords, each with a constraint (a required letter and the range of allowed counts for that
-   letter), determine how many passwords satisfy their constraint."
+  "Given a list of passwords, each with a constraint (a required letter and
+  the range of allowed counts for that letter), determine how many passwords
+  satisfy their constraint."
   [orig-input]
   (part1-impl (parse-input orig-input)))
 
+(defn is-valid-part2
+  [pwi]
+  (let [letter (.letter pwi)
+        password (.password pwi)
+        ; Indices are one-based, hence the `dec`s to convert to zero-based.
+        letter-x (nth password (dec (.x pwi)))
+        letter-y (nth password (dec (.y pwi)))]
+    (not=
+     (= letter letter-x)
+     (= letter letter-y))))
+
+(defn part2-impl
+  [pwis]
+  (count (filter is-valid-part2 pwis)))
+
 (defn part2
+  "Given a list of passwords, each with a constraint (a required letter and
+  two indices such that the letter must be at exactly one of the two
+  indices), determine how many passwords satisfy their constraint."
   [orig-input]
-  ())
+  (part2-impl (parse-input orig-input)))
--- test/aoc_2020/day02_test.clj
+++ test/aoc_2020/day02_test.clj
@@ -1,6 +1,6 @@
 (ns aoc-2020.day02-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc-2020.day02 :refer [part1]]))
+            [aoc-2020.day02 :refer [part1 part2]]))
 
 (def test-input "1-3 a: abcde
 1-3 b: cdefg
@@ -10,3 +10,7 @@
   (testing "part1 finds how many passwords contain a valid count of their
             given letter"
     (is (= (part1 test-input) 2))))
+
+(deftest part2-test
+  (testing "part2 finds how many passwords contain the letter at exactly one of the two indices"
+    (is (= (part2 test-input) 1))))

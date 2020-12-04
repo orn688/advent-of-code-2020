674bf0d8a67bdeb5f5fe69380f1488e23bd8e47d
Oliver Newman <orn688@gmail.com>
2020-12-03 23:32:27 -0500

    Add project URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aoc-2020 "0.1.0-SNAPSHOT"
   :description "Advent of Code 2020 solutions in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/orn688/advent-of-code-2020"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "3.10.3"]]
   :repl-options {:init-ns aoc-2020.core}

923f69b904aa32b326559f022afd17e56db76a62
Oliver Newman <orn688@gmail.com>
2020-12-05 19:46:08 -0500

    Complete day 5, part 2

--- src/aoc_2020/day05.clj
+++ src/aoc_2020/day05.clj
@@ -10,12 +10,27 @@
 
 (defn parse-input
   [orig-input]
-  (str/split-lines (str/trim orig-input)))
+  (map seat-id (str/split-lines (str/trim orig-input))))
 
 (defn part1
   "Determines the maximum seat ID among all the seat strings."
   [orig-input]
-  (apply max (map seat-id (parse-input orig-input))))
+  (apply max (parse-input orig-input)))
+
+(defn find-missing-id
+  [seat-ids index]
+  (if (= index (dec (count seat-ids)))
+    (throw (Exception. "no missing seat ID found"))
+    (let [curr (nth seat-ids index)
+          next (nth seat-ids (inc index))
+          diff (- next curr)]
+      (case diff
+        1 (find-missing-id seat-ids (inc index))
+        2 (inc curr)
+        (throw (Exception. (format "bad diff between sead ids %d and %d" curr next)))))))
 
 (defn part2
-  [_])
+  "Determines the narrator's seat ID (the only seat ID x such that x+1 and
+  x-1 are present, but x is not)."
+  [orig-input]
+  (find-missing-id (vec (sort (parse-input orig-input))) 0))
--- test/aoc_2020/day05_test.clj
+++ test/aoc_2020/day05_test.clj
@@ -1,6 +1,7 @@
 (ns aoc-2020.day05-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [aoc-2020.day05 :refer [part1 seat-id]]))
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is testing]]
+            [aoc-2020.day05 :refer [part1 part2 seat-id]]))
 
 (deftest seat-id-test
   (testing "seat-id returns the correct seat ID"
@@ -8,7 +9,7 @@
     (is (= (seat-id "FFFBBBFRRR") 119))
     (is (= (seat-id "BBFFBBFRLL") 820))))
 
-(def test-input "
+(def test-input-part1 "
 BFFFBBFRRR
 FFFBBBFRRR
 BBFFBBFRLL
@@ -16,4 +17,22 @@ BBFFBBFRLL
 
 (deftest part1-test
   (testing "part1"
-    (is (= (part1 test-input) 820))))
+    (is (= (part1 test-input-part1) 820))))
+
+(def test-input-part2
+  (str/join "\n" (shuffle (map
+                           (partial str "FFFFFF")
+                           ["FRRR" ; 7
+                            "BLLL" ; 8
+                            "BLLR" ; 9
+                            "BLRL" ; 10
+                            ; Missing 11
+                            "BRLL" ; 12
+                            "BRLR" ; 13
+                            "BRRL" ; 14
+                            "BRRR" ; 15
+                            ]))))
+
+(deftest part2-test
+  (testing "part2"
+    (is (= (part2 test-input-part2) 11))))

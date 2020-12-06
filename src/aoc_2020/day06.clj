81a167b449f39d74a21448f16fde6e0df6593bbe
Oliver Newman <orn688@gmail.com>
2020-12-06 00:29:55 -0500

    Complete day 6, part 2

--- src/aoc_2020/day06.clj
+++ src/aoc_2020/day06.clj
@@ -1,5 +1,19 @@
 (ns aoc-2020.day06
+  (:require [clojure.set])
   (:require [clojure.string :as str]))
+
+(defn parse-group
+  [raw-group]
+  (->> raw-group
+       str/split-lines
+       (map set)))
+
+(defn parse-input
+  [orig-input]
+  (->> orig-input
+       str/trim
+       (#(str/split % #"\n\n"))
+       (map parse-group)))
 
 (defn part1
   "Returns the sum of the number of unique questions that any member of each
@@ -14,5 +28,17 @@
        (map count)
        (reduce +)))
 
+(defn count-shared-yeses
+  [group]
+  (->> group
+       (apply clojure.set/intersection)
+       count))
+
 (defn part2
-  [_])
+  "Returns the sum of the number of unique questions that *all* members of
+  each group answered 'yes' to."
+  [orig-input]
+  (->> orig-input
+       parse-input
+       (map count-shared-yeses)
+       (reduce +)))
--- test/aoc_2020/day06_test.clj
+++ test/aoc_2020/day06_test.clj
@@ -1,6 +1,6 @@
 (ns aoc-2020.day06-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc-2020.day06 :refer [part1]]))
+            [aoc-2020.day06 :refer [part1 part2]]))
 
 (def test-input "
 abc
@@ -23,3 +23,7 @@ b
 (deftest part1-test
   (testing "part1"
     (is (= (part1 test-input) 11))))
+
+(deftest part2-test
+  (testing "part2"
+    (is (= (part2 test-input) 6))))

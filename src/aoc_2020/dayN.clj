b94f7d00e0727598eb907536ef6b68e639309f85
Oliver Newman <orn688@gmail.com>
2020-12-13 18:10:34 -0500

    Add template files

---
+++ src/aoc_2020/dayN.clj
@@ -0,0 +1,9 @@
+(ns aoc-2020.dayN
+  (:require [clojure.string :as str]))
+
+(defn part1
+  [input]
+  ())
+
+(defn part2
+  [_])
---
+++ test/aoc_2020/dayN_test.clj
@@ -0,0 +1,10 @@
+(ns aoc-2020.dayN-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [aoc-2020.dayN :refer [part1 part2]]))
+
+(def test-input "
+")
+
+(deftest part1-test
+  (testing "part1"
+    (is (= (part1 test-input) ()))))
